0c710817aa6b352b4764fdf6d4f1e900ce0378b7
aequey <antoine.equey@epfl.ch>
2017-10-02 11:31:01 +0200

    Added class Neuron, main, CMakeLists.txt and an executable.

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "neuron.cpp"
+
+int main() {
+	
+	std::cout << "Test" << std::endl;
+	
+	return 0;
+}
---
+++ src/neuron.h
@@ -0,0 +1,13 @@
+#ifndef NEURON_H
+#define nEURON_H
+
+#include <iostream>
+
+class Neuron {
+public :
+	
+private :
+	
+};
+
+#endif // NEURON_H
