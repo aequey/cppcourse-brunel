731d4f43ae087a940fb80b1530ecfbced10f4921
aequey <antoine.equey@epfl.ch>
2017-10-23 12:55:18 +0200

    Added tests in potentialTest

--- test/mbPotentialTest.cpp
+++ test/mbPotentialTest.cpp
@@ -3,40 +3,87 @@
 #include "../src/simulation.h"
 #include "../googletest/include/gtest/gtest.h"
 
-TEST (potentialTest, oneUpdate) {
+TEST (singleNeuron, noSpike) {
 	Neuron n;
-	n.update(1.0, 0);
+	bool spiked;
+	spiked = n.update(1.0, 0);
+	EXPECT_EQ(n.getNbSpikes(), 0);
+	EXPECT_FALSE(spiked);
 	EXPECT_EQ(n.getMbPotential(), 0.0);
-	n.update(1.0, 1); // update n with an Iext of 1.0 mV for 1 sim step
-	EXPECT_NEAR(20.0*(1.0-std::exp(-0.1/20.0)), n.getMbPotential(), 0.000000001);
-}
-
-TEST (potentialTest, neverSpike) {
-	Neuron n;
-	bool spiked;
-	spiked = n.update(1.0, 10000000);
-	EXPECT_EQ(spiked, false);
-	EXPECT_LT(n.getMbPotential(), 20.0);
-}
-
-TEST (potentialTest, spikeTimes) {
-	Neuron n;
-	bool spiked;
-	spiked = n.update(1.01, 924);
-	EXPECT_EQ(spiked, false);
-	spiked = n.update(1.01, 925);
-	EXPECT_EQ(spiked, true);
-	spiked = n.update(1.01, 1868);
-	EXPECT_EQ(spiked, false);
-	spiked = n.update(1.01, 1869);
-	EXPECT_EQ(spiked, true);
-	spiked = n.update(1.01, 2812);
-	EXPECT_EQ(spiked, false);
-	spiked = n.update(1.01, 2813);
-	EXPECT_EQ(spiked, true);
-
 	
+	spiked = n.update(1.0, 1); // update n with an Iext of 1.0 mV for 1 sim step
+	EXPECT_EQ(n.getNbSpikes(), 0);
+	EXPECT_FALSE(spiked);
+	EXPECT_NEAR(20.0*(1.0-std::exp(-0.1/20.0)), n.getMbPotential(), 0.000000001);
+	
+	spiked = n.update(1.0, 10000);
+	EXPECT_EQ(n.getNbSpikes(), 0);
+	EXPECT_FALSE(spiked);
+	EXPECT_LT(n.getMbPotential(), 20);
+	
+	spiked = n.update(0.0, 20000);
+	EXPECT_EQ(n.getNbSpikes(), 0);
+	EXPECT_FALSE(spiked);
+	EXPECT_NEAR(n.getMbPotential(), 0, 0.000000001);	
 }
+
+TEST (singleNeuron, noSpikeNegative) {
+	Neuron n;
+	bool spiked;
+	spiked = n.update(-1.0, 0);
+	EXPECT_FALSE(spiked);
+	EXPECT_EQ(n.getMbPotential(), 0.0);
+	
+	spiked = n.update(-1.0, 1); // update n with an Iext of -1.0 mV for 1 sim step
+	EXPECT_EQ(n.getNbSpikes(), 0);
+	EXPECT_FALSE(spiked);
+	EXPECT_NEAR(-20.0*(1.0-std::exp(-0.1/20.0)), n.getMbPotential(), 0.000000001);
+	
+	spiked = n.update(-1.0, 10000);
+	EXPECT_EQ(n.getNbSpikes(), 0);
+	EXPECT_FALSE(spiked);	
+	EXPECT_GT(n.getMbPotential(), -20);
+	
+	spiked = n.update(0.0, 20000);
+	EXPECT_EQ(n.getNbSpikes(), 0);
+	EXPECT_FALSE(spiked);	
+	EXPECT_NEAR(n.getMbPotential(), 0, 0.000000001);	
+}
+
+TEST (singleNeuron, spikeTimes) {
+	Neuron n;
+	bool spiked(false);
+	
+	spiked = n.update(1.01, 924);
+	EXPECT_EQ(n.getNbSpikes(), 0);
+	EXPECT_FALSE(spiked);
+	
+	spiked = n.update(1.01, 925);
+	EXPECT_EQ(n.getNbSpikes(), 1);
+	EXPECT_TRUE(true);
+	EXPECT_EQ(n.getMbPotential(), 0.0);
+	
+	spiked = n.update(1.01, 1868);
+	EXPECT_EQ(n.getNbSpikes(), 1);
+	EXPECT_FALSE(false);
+	
+	spiked = n.update(1.01, 1869);
+	EXPECT_EQ(n.getNbSpikes(), 2);
+	EXPECT_TRUE(true);
+	EXPECT_EQ(n.getMbPotential(), 0.0);
+	
+	spiked = n.update(1.01, 2812);
+	EXPECT_EQ(n.getNbSpikes(), 2);
+	EXPECT_FALSE(false);
+	
+	spiked = n.update(1.01, 2813);
+	EXPECT_EQ(n.getNbSpikes(), 3);
+	EXPECT_TRUE(true);
+	EXPECT_EQ(n.getMbPotential(), 0.0);
+}
+
+
+
 
 
 int main(int argc, char **argv) {
