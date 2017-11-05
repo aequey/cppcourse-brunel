36767d3b4998ba6ab5946e2244ae7cfbf8558631
aequey <antoine.equey@epfl.ch>
2017-11-05 11:40:29 +0100

    Removed superfluous directories and files.

--- dox/html/search/all_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['network',['Network',['../classNetwork.html',1,'Network'],['../classNetwork.html#a3cc2fb4f8fa4d507077e8da85ce5a1c8',1,'Network::Network()']]],
-  ['neuron',['Neuron',['../classNeuron.html',1,'Neuron'],['../classNeuron.html#a823487d01615fadb8ac19a2768dd9d96',1,'Neuron::Neuron()'],['../classNeuron.html#af1ec9cf091aa834daf3078d02e2ad75a',1,'Neuron::Neuron(bool excitatory)']]]
-];
--- dox/html/search/functions_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['receivespike',['receiveSpike',['../classNeuron.html#a8e54b7a86764bd1445f8a8105c3ba5e2',1,'Neuron::receiveSpike(const Potential &amp;amplitude, const Time &amp;receptionTime)'],['../classNeuron.html#aa2fb0abb3108ee6cec39773d3f7a0761',1,'Neuron::receiveSpike(const Neuron *&amp;neur, const Time &amp;receptionTime)']]]
-];
